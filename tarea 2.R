fb58c471c5d13d9bc71c40b5588b96e5b9c24870
christian Bernalgit config --global user.email christianrbernal@hotmail.es <you@example.com>
2020-02-19 09:08:19 -0600

    actualizar

--- tarea 2.R
+++ tarea 2.R
@@ -1,9 +1,26 @@
 library(plyr)
-accidentes <- c(0,1,0,2,2,1,4,3,0,1,5,1,2,3,4,9,1,1,3,4)
-accidentes <- count(accidentes)
-accidentes
+accidentes <- c(0,1,0,2,2,1,4,3,0,1,5,1,2,3,4,0,1,1,3,4)
+acc <- count(accidentes)
+acc
 
-(accidentes$freq/sum(accidentes$freq)*100)
-accidentes$rf <- accidentes$freq/sum(accidentes$freq)*100
+acc$fr <- acc$freq/sum(acc$freq)*100
+acc
 
-barplot(accidentes$freq, names.arg = accidentes$x, main = "accidentes en el aserradero" , col = "red")
+barplot(acc$freq, names.arg = acc$x, main = "accidentes en el aserradero", col = "red")
+mean(accidentes)
+sum(accidentes)
+boxplot(accidentes)
+
+# Ejercicio 2 -------------------------------------------------------------
+
+especies <- c("F", "H", "F", "C", "F", "A", "H", "F",
+              "H", "C", "A", "C", "F", "H", "H", "H",
+              "F", "H", "A", "C", "F", "H", "H", "F")
+
+esp <- count(especies)
+esp
+
+esp$fr <- esp$freq/sum(esp$freq)*100
+esp
+
+barplot(esp$freq, names.arg = esp$x, main = "especies", col = "blue")
