4196d4dd0101fc594e73ee42e4bdea62bca01e4f
christian Bernalgit config --global user.email christianrbernal@hotmail.es <you@example.com>
2020-02-20 08:25:23 -0600

    actualizar

--- tarea 2.R
+++ tarea 2.R
@@ -24,3 +24,43 @@ esp$fr <- esp$freq/sum(esp$freq)*100
 esp
 
 barplot(esp$freq, names.arg = esp$x, main = "especies", col = "blue")
+
+
+# ejercicio 3 -------------------------------------------------------------
+
+library(repmis)
+conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
+
+vecyesp <- table(conjunto$Vecinos, conjunto$Especie)
+vecyesp
+
+
+
+
+# ejercicio 4 -------------------------------------------------------------
+
+altura <- (conjunto$Altura)
+range(altura)
+
+Intervalo <- seq(8, 21, by=4)
+Intervalo
+
+altura.table <- cut(altura, Intervalo)
+table(altura.table)
+
+altura.prop <- cbind(table(altura.table))
+altura.per <- round(prop.table(altura.prop)*100,2)
+
+
+# ejercicio 5 -------------------------------------------------------------
+
+diametro <- conjunto$Diametro
+range(diametro)
+
+Intervalo <- seq(7.5, 25.5, by=2.5)
+Intervalo
+
+par(mfrow=c(1,2))
+hist(conjunto$Diametro, main = "sin modificar", xlab = "diametro", col = "blue")
+hist(conjunto$Diametro, breaks = Intervalo, main = "datos intervalos", col = "red")
+par(mfrow=c(1,1))
